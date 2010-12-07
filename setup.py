53a8c35f5334ec2a4d36ef898ed97c01860d1154
Nelson Altimari <nelson@whitehat.com.br>
2010-12-07 17:24:12 -0200

    packaging for pip install

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "filebrowser",
+    version = "0.9-whitehat",
+    packages = find_packages(),
+    
+    include_package_data = True,
+
+    author = "Nelson Altimari",
+    author_email = "nelson@whitehat.com.br",
+    description = "Filebrowser fork",
+    license = "MIT License",
+)
